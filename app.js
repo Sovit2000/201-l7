b416b79c39b9e106714fc5cad009ab661c9ab356
Sovit <sovit.202104006@tulas.edu.in>
2022-12-27 23:08:07 +0530

    updated

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
-var csrf = require("tiny-csrf");
-const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
-app.use(express.urlencoded({ extended: false }));
 const path = require("path");
-
-app.use(bodyParser.json());
-app.use(cookieParser("ssh!!!! some secret string"));
-app.use(csrf("123456789iamasecret987654321look", ["POST", "PUT", "DELETE"]));
+var csrf = require("tiny-csrf");
 
 // seting the ejs is the engine
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser("ssh!!!! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+
+const { Todo, User } = require("./models");
 
 app.get("/", async (request, response) => {
   const allTodos = await Todo.getTodos();
@@ -41,6 +41,22 @@ app.use(express.static(path.join(__dirname, "public")));
 //Signup page
 app.get("/signup", (request, response) => {
   response.render("signup", { title: "Signup", csrfToken: request.csrfToken()})
+})
+
+app.post("/users", async (request, response) => {
+  console.log("Firstname", request.body.firstName)
+  // Have to create the user here
+  try{
+    const user = await User.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: request.body.password
+    });
+    response.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 })
 
 app.get("/todos", (request, response) => {
